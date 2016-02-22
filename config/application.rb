239e4b95de7dd3ef31b8b956ab42e4ef0e3d487c
Yerko Palma <yerko.palma@usach.cl>
2016-02-22 15:33:53 +0000

    fixed sqlite dependency
    removed sqlite dependency from config files

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,10 @@
 require File.expand_path('../boot', __FILE__)
 
-require 'rails/all'
+# require 'rails/all'
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -23,6 +27,6 @@ module Workspace
       g.orm :mongoid
     end
     # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
+    # config.active_record.raise_in_transactional_callbacks = true
   end
 end
