a9a0bf1e2d61cc552ec16b6b55fbeab46b1bfd79
Yerko Palma <yerko.palma@usach.cl>
2016-02-25 20:56:15 +0000

    [ci skip] added more tests for idea controller (failing)

--- test/controllers/ideas_controller_test.rb
+++ test/controllers/ideas_controller_test.rb
@@ -13,7 +13,9 @@ class IdeasControllerTest < ActionController::TestCase
   end
 
   test 'should get index.html' do
-
+    get :index
+    assert_response :success
+    assert_not_includes @response["Content-Type"], "application/json"
   end
 
   test 'should get index.json' do
@@ -23,7 +25,20 @@ class IdeasControllerTest < ActionController::TestCase
     assert_not_nil assigns(:ideas)
   end
 
+  test "should create user with json format" do
+    assert_difference('Idea.count') do
+      # can not pass the Idea object. Need to pass a hash,
+      # so it would be better to create a function in test_helper
+      # that takes a model object and return a hash (for ideas and canvas)
+      post :create, { idea: @easycount, format: :json }
+      assert_includes @response["Content-Type"], "application/json"
+    end
+  end
+
   test 'should get show.json' do
+    get :show, { id: @easycount.name, format: :json}
+    assert_response :success
+    assert_includes @response["Content-Type"], "application/json"
   end
 
   test 'should not get show.html' do
