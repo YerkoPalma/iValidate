20c4cb5ae360062157bd803c4d5d09c185cd8532
Yerko Palma <yerko.palma@usach.cl>
2016-02-25 19:16:54 +0000

    controller tests set up

--- test/controllers/ideas_controller_test.rb
+++ test/controllers/ideas_controller_test.rb
@@ -1,8 +1,31 @@
 require 'test_helper'
 
 class IdeasControllerTest < ActionController::TestCase
-  test 'should get index' do
-    get :index
+  def setup
+    @easycount = build(:easycount)
+    @easycount.save
+    @fixar = build(:fixar)
+    @fixar.save
+  end
+
+  def teardown
+    Idea.delete_all
+  end
+
+  test 'should get index.html' do
+
+  end
+
+  test 'should get index.json' do
+    get :index, {format: :json}
     assert_response :success
+    assert_includes @response["Content-Type"], "application/json"
+    assert_not_nil assigns(:ideas)
+  end
+
+  test 'should get show.json' do
+  end
+
+  test 'should not get show.html' do
   end
 end
