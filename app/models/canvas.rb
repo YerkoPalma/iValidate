9cc19ebc0b95fe90aab851ad6b1b70ad4432db1f
Yerko Palma <yerko.palma@usach.cl>
2016-02-25 16:52:28 +0000

    Canvas model tests
    refactor of canvas model tests, fixed factories for canvas model

--- app/models/canvas.rb
+++ app/models/canvas.rb
@@ -3,6 +3,10 @@
 # Author::    Yerko Palma  (mailto:yerko.palma@usach.cl)
 class Canvas
   include Mongoid::Document
+  include Mongoid::EmbeddedErrors
+
+  validate :description_and_tags_present, :descriptions_must_be_strings,
+            :tags_should_be_non_empty_string_arrays, :uniq_tags
 
   field :key_partners, type: Hash
   field :key_activities, type: Hash
@@ -14,5 +18,78 @@ class Canvas
   field :cost_structures, type: Hash
   field :revenue_streams, type: Hash
 
+  validates :key_partners, presence: true
+  validates :key_activities, presence: true
+  validates :key_resources, presence: true
+  validates :value_propositions, presence: true
+  validates :customer_relationships, presence: true
+  validates :channels, presence: true
+  validates :customer_segments, presence: true
+  validates :cost_structures, presence: true
+  validates :revenue_streams, presence: true
+
+  def description_and_tags_present
+    self.attributes.each do |prop, value|
+      if prop != '_id' && !value.nil?
+        if !value.has_key? :description 
+          errors.add(prop, 'Must have a description field')
+        end
+
+        if value[:description].nil?
+          errors.add(prop, 'Description can not be nil')
+        end
+
+        if !value.has_key? :tags || value[:tags].nil?
+          errors.add(prop, 'Must have a tags field')
+        end
+
+        if value[:tags].nil?
+          errors.add(prop, 'Tags can not be nil')
+        end
+      end
+    end
+  end
+
+  def descriptions_must_be_strings
+    self.attributes.each do |prop, value|
+      if prop != '_id' && !value.nil?
+        if !value[:description].instance_of? String
+          errors.add(prop, 'description must be a string')
+        end
+      end
+    end
+  end
+
+  def tags_should_be_non_empty_string_arrays
+    self.attributes.each do |prop, value|
+      if prop != '_id' && !value.nil?
+        if !value[:tags].instance_of? Array
+          errors.add(prop, 'tags must be Array')
+        else
+          if !value[:tags].any?
+            errors.add(prop, "tags can not be empty")
+          else
+            value[:tags].each do |tag|
+              if !tag.instance_of? String
+                errors.add(prop, "must have only string tags")
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+
+  def uniq_tags
+    self.attributes.each do |prop, value|
+      if prop != '_id' && !value.nil?
+        if value[:tags].instance_of? Array
+          if !(value[:tags].uniq.length == value[:tags].length)
+            errors.add(prop, "must have unique tags")
+          end
+        end
+      end
+    end
+  end
   embedded_in :idea, class_name: 'Idea', inverse_of: :canvas
 end
--- test/models/canvas_test.rb
+++ test/models/canvas_test.rb
@@ -1,6 +1,9 @@
 require 'test_helper'
 
 class CanvasTest < ActiveSupport::TestCase
+  @canvas_properties = %w[key_partners key_activities key_resources 
+                            value_propositions customer_relationships channels 
+                            customer_segments cost_structures revenue_streams]
   def setup
     # to prevent duplicate ideas
     Idea.delete_all
@@ -22,101 +25,50 @@ class CanvasTest < ActiveSupport::TestCase
     assert_not @easycount.save, 'Saved empty idea'
   end
 
-  test 'key_partners should be present' do
-    @easycount.canvas.key_partners = nil
-    assert_not @easycount.canvas.valid?
-    @easycount.canvas.key_partners = { description: 'New key partners',
-                                tags: %w[my partners tags] }
-    assert @easycount.canvas.valid?, @easycount.errors.full_messages[0]
-  end
+  @canvas_properties.each do |property|
+    test "#{property} should be present" do
+      # all properties are required
+      @easycount.canvas[property] = nil
+      assert_not @easycount.canvas.valid?
+      @easycount.canvas[property] = { description: 'New key partners',
+                                  tags: %w[my partners tags] }
+      assert @easycount.canvas.valid?, @easycount.errors.full_messages[0]
+    end
+    
+    test "#{property} description should be a string" do
+      # all properties must have a description
+      @easycount.canvas[property][:description] = nil
+      assert_not @easycount.canvas.valid?, @easycount.canvas.errors.full_messages.to_json
+      # descriptions can't be numbers
+      @easycount.canvas[property][:description] = 1
+      assert_not @easycount.canvas.valid?, @easycount.canvas.errors.full_messages.to_json
+      @easycount.canvas[property][:description] = 'Valid description'
+      assert @easycount.canvas.valid?, @easycount.canvas.errors.full_messages.to_json
+      assert_instance_of String, @easycount.canvas[property][:description]
+    end
 
-  test 'key_partners should have description and tags properly set' do
-    # description must be a string
-    assert_not @easycount.canvas.key_partners[:description].nil?
-    assert_instance_of String, @easycount.canvas.key_partners[:description]
-    @easycount.canvas.key_partners[:description] = 0
-    assert_not @easycount.canvas.valid?
+    test "#{property} tags should be an array of strings" do
+      # all properties must have tags
+      @easycount.canvas[property][:tags] = nil
+      assert_not @easycount.canvas.valid?, 'tags not present'
+      # tags can't be strings
+      @easycount.canvas[property][:tags] = 'not valid tag value'
+      assert_not @easycount.canvas.valid?, 'tags must be arrays'
+      # tags must be arrays...
+      @easycount.canvas[property][:tags] = []
+      assert_instance_of Array, @easycount.canvas[property][:tags]
+      # ...non empty arrays
+      assert_not @easycount.canvas.valid?, 'there must be at least one tag'
+      # ..of strings
+      @easycount.canvas[property][:tags] = [0, 1, 2]
+      assert_not @easycount.canvas.valid?, 'tags should be strings'
+    end
 
-    # tags should be an array of strings
-    assert_not @easycount.canvas.key_partners[:tags].nil?
-    assert_instance_of Array, @easycount.canvas.key_partners[:tags]
-    @easycount.canvas.key_partners[:tags] = [0, 1, 2]
-    assert_not @easycount.canvas.valid?
-
-    # tags can not be repeated
-    @easycount.canvas.key_partners[:tags] = %w[tag1 tag2 tag1]
-    assert_not @easycount.canvas.valid?
-  end
-
-  test 'key_activities should be present' do
-  end
-
-  test 'key_activities should have description and tags properly set' do
-    # description must be a string
-    # tags should be an array of strings
-    # tags can be repeated
-  end
-
-  test 'key_resources should be present' do
-  end
-
-  test 'key_resources should have description and tags properly set' do
-    # description must be a string
-    # tags should be an array of strings
-    # tags can be repeated
-  end
-
-  test 'value_propositions should be present' do
-  end
-
-  test 'value_propositions should have description and tags properly set' do
-    # description must be a string
-    # tags should be an array of strings
-    # tags can be repeated
-  end
-
-  test 'customer_relationships should be present' do
-  end
-
-  test 'customer_relationships should have description and tags properly set' do
-    # description must be a string
-    # tags should be an array of strings
-    # tags can be repeated
-  end
-
-  test 'channels should be present' do
-  end
-
-  test 'channels should have description and tags properly set' do
-    # description must be a string
-    # tags should be an array of strings
-    # tags can be repeated
-  end
-
-  test 'customer_segments should be present' do
-  end
-
-  test 'customer_segments should have description and tags properly set' do
-    # description must be a string
-    # tags should be an array of strings
-    # tags can be repeated
-  end
-
-  test 'cost_structures should be present' do
-  end
-
-  test 'cost_structures should have description and tags properly set' do
-    # description must be a string
-    # tags should be an array of strings
-    # tags can be repeated
-  end
-
-  test 'revenue_streams should be present' do
-  end
-
-  test 'revenue_streams should have description and tags properly set' do
-    # description must be a string
-    # tags should be an array of strings
-    # tags can be repeated
+    test "#{property} tags must be unique" do
+      @easycount.canvas[property][:tags] = %w[tag1 tag2 tag1]
+      assert_not @easycount.canvas.valid?
+      @easycount.canvas[property][:tags] = %w[this are valid tags]
+      assert @easycount.canvas.valid?
+    end
   end
 end
