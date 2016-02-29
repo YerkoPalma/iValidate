84e084f8aa0245c6b8283e9422b5b6255c2641a5
Yerko Palma <yerko.palma@usach.cl>
2016-02-29 13:01:50 +0000

    updated ideas controller tests

--- test/controllers/ideas_controller_test.rb
+++ test/controllers/ideas_controller_test.rb
@@ -3,9 +3,9 @@ require 'test_helper'
 class IdeasControllerTest < ActionController::TestCase
   def setup
     @easycount = build(:easycount)
-    @easycount.save
+    # @easycount.save
     @fixar = build(:fixar)
-    @fixar.save
+    # @fixar.save
   end
 
   def teardown
@@ -27,20 +27,37 @@ class IdeasControllerTest < ActionController::TestCase
 
   test "should create user with json format" do
     assert_difference('Idea.count') do
-      # can not pass the Idea object. Need to pass a hash,
-      # so it would be better to create a function in test_helper
-      # that takes a model object and return a hash (for ideas and canvas)
-      post :create, { idea: @easycount, format: :json }
+      post :create, { idea: Hash[@easycount.attributes], format: :json }
       assert_includes @response["Content-Type"], "application/json"
+      assert_response :created
     end
   end
 
   test 'should get show.json' do
+    # ensure that the idea is created on db
+    post :create, { idea: Hash[@easycount.attributes], format: :json }
     get :show, { id: @easycount.name, format: :json}
     assert_response :success
     assert_includes @response["Content-Type"], "application/json"
   end
 
   test 'should not get show.html' do
+    # ensure that the idea is created on db
+    post :create, { idea: Hash[@easycount.attributes], format: :json }
+    assert_raises ActionController::UnknownFormat do
+      get :show, id: @easycount.name
+    end
+  end
+
+  test 'should create embeded canvas model' do
+    # ensure that the idea is created on db
+    post :create, { idea: Hash[@easycount.attributes], format: :json }
+    post :model, {id: @easycount.name, canvas: Hash[build(:easycount_canvas).attributes], format: :json}
+    # check if the model was saved correctly
+    easycount_model = Idea.find(@easycount.name)
+    assert_not_nil easycount_model.canvas
+    assert easycount_model.canvas.valid?
+    assert_includes @response["Content-Type"], "application/json"
+    assert_response :created
   end
 end
